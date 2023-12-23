207b219842c64abe9ffdab5c8e7027c0e82fdfa8
Chiranjibi Karki <ckarki70@gmail.com>
2023-12-23 11:35:40 -0500

    mongodb and mongoose setup

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express=require("express");
 const cors=require("cors");
+const mongoose=require("mongoose");
 const app=express();
 
 app.use(cors());
 app.use(express.json());
 
-app.listen(3001,()=>{
-    console.log("Server running on port 3001");
-})
+async function main(){
+    await mongoose.connect("mongodb+srv://Chiranjibi:Chiranjibi1234@bmiapi.jwmwkcx.mongodb.net/?retryWrites=true&w=majority").then(()=>{
+        console.log("MongoDB connection successfull!");
+        app.listen(3001,()=>{
+            console.log("Server running on port 3001");
+        })
+    }).catch(err=>console.log(err));
+};
+
+main();
+
